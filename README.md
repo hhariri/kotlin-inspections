b7ba0d070495bc6057f26bc6d0201aad54ba5f3a
hadihariri <mail@hadihariri.com>
2017-05-30 07:56:37 +0200

    Sample inspections

---
+++ README.md
@@ -0,0 +1,15 @@
+## Sample project
+
+A sample project demonstrating how to run Inspections and Lint tools on the server 
+
+For more information see the following blog post:
+
+
+{{ do remember to insert the blog post here* }}
+
+
+
+
+
+\* If I've forgotten, it's somewhere on my site https://hadihariri.com
+
---
+++ src/main/kotlin/com/hadihariri/kotlin/inspections/Main.kt
@@ -0,0 +1,30 @@
+package com.hadihariri.kotlin.inspections
+
+import java.math.BigDecimal
+
+
+fun sum(x: Int, y: Int) = x + y
+
+fun returningUnit(param: String): Unit {
+
+}
+
+private val Int.bd: BigDecimal
+    get() {
+        return BigDecimal(this)
+    }
+
+fun main(args: Array<String>) {
+    val unusedVar = 100
+
+    val redundantSemicolon = 200;
+
+
+    println(redundantSemicolon)
+
+}
+
+fun usingTwoSpaces() {
+  val someValue = "At least you're not using TABS!"
+  println("But you insist on two spaces!")
+}
